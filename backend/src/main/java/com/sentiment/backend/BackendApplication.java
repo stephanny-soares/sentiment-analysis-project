3ff5d79506c2987f4f24c525d77ab1863eac86ed
MrClaro <adryan.contatoprofissional@gmail.com>
2026-01-12 21:30:09 -0300

    refactor(backend/app): adiciona @EnableConfigurationProperties
    - Adiciona anotação para garantir processamento de
    @ConfigurationProperties
    - Melhora explicitação de configurações

--- backend/src/main/java/com/sentiment/backend/BackendApplication.java
+++ backend/src/main/java/com/sentiment/backend/BackendApplication.java
@@ -2,12 +2,14 @@ package com.sentiment.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BackendApplication.class, args);
+  }
 
 }
